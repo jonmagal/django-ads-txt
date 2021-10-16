5643ad61dffca5bee46b923f41477c6e3e942e62
Jonathas Magalhães <jonathas@recsalt.com>
2021-10-16 08:37:34 -0300

    change url in favor of the re_path

--- ads_txt/urls.py
+++ ads_txt/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from ads_txt.views import rules_list
 
 urlpatterns = [
-    url(r'^$', rules_list, name='ads_txt_rule_list'),
+    re_path(r'^$', rules_list, name='ads_txt_rule_list'),
 ]
